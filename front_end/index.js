f5697ae179cd010dab875f82e1fbc8cfcc5b2126
j2002chen <j.2002chen@gmail.com>
2021-10-24 18:35:09 -0400

    Updated front end to fix cors issue

--- front_end/index.js
+++ front_end/index.js
@@ -2,14 +2,13 @@ function grabQuote(){
   fetch('http://127.0.0.1:5000/ai-quotes', { 
     method: 'GET', // *GET, POST, PUT, DELETE, etc.
     cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
     redirect: 'follow', // manual, *follow, error
     referrerPolicy: 'no-referrer' // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-  })
+  }).then(response => response.json())
   .then(data => {
       console.log(data)
+      const obj = data;
+      document.getElementById("1").innerHTML = obj.author;
+      document.getElementById("2").innerHTML = obj.quote;
   });
 }
