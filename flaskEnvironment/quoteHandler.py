89659e466d3a5f3bceb2faed421ad1538e29d071
j2002chen <j.2002chen@gmail.com>
2021-10-24 19:16:02 -0400

    Fixed errors for unit testing

--- flaskEnvironment/quoteHandler.py
+++ flaskEnvironment/quoteHandler.py
@@ -1,13 +1,10 @@
 from ai_quotes import ai_quotes 
-from unit_tests import *
 import random
 
 def randomQuote():
-    i = 1
     return random.choice(ai_quotes), 200
 
 def selectQuote(id):
-    j = 1
     for quote in ai_quotes:
         if(quote["id"] == id):
             return quote, 200
