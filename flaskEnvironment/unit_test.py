8e2e3960cdf1630c2fe2d625708f0011cc37b248
j2002chen <j.2002chen@gmail.com>
2021-10-27 13:18:00 -0400

    Fixed unit testing for app functionality

--- flaskEnvironment/unit_test.py
+++ flaskEnvironment/unit_test.py
@@ -2,16 +2,14 @@ from app import *
 from quoteHandler import *
 from ai_quotes import ai_quotes
 
-class TestClass:
-
-    def test_randomQuotes():
-        assert ai_quotes.contains(randomQuote())
-        
-    def test_selectQuote():
-        for id in range(0,9):
-            success = False
-            for quote in ai_quotes:
-                if(quote["id"] == id):
-                    success = True
-            assert success == True
+def test_randomQuotes():
+    assert randomQuote() in ai_quotes == True
+           
+def test_selectQuote():
+    for id in range(0,9):
+        success = False
+        for quote in ai_quotes:
+            if(quote["id"] == id):
+                success = True
+        assert success == True
 
